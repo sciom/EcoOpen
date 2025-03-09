a3d182d52cccefbc650f3a4eb9ee7ccf36531708
Domagoj K. Hackenberger <domagojhack@gmail.com>
2025-03-09 09:49:51 +0100

    preparing new example folders

--- EcoOpenPy/setup.py
+++ EcoOpenPy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="EcoOpen",
-    version="0.0.52",
+    version="0.0.53",
     packages=find_packages(),
     install_requires=[
         "habanero",

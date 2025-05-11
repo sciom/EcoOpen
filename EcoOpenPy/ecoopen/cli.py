2027941795a7351d27749cd48b70cf4e112a3da3
Domagoj K. Hackenberger <domagojhack@gmail.com>
2025-05-11 17:57:03 +0200

    some fixes

--- EcoOpenPy/ecoopen/cli.py
+++ EcoOpenPy/ecoopen/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from EcoOpenPy.ecoopen import process_and_analyze_dois  # Absolute import
+from ecoopen.ecoopen import process_and_analyze_dois, DEFAULT_DATA_FORMATS  # Absolute import
 
 def main():
     """Command-line interface for ecoopen package."""
@@ -22,6 +22,29 @@ def main():
         default=True,
         help="Save downloaded PDFs to disk (default: True)"
     )
+    parser.add_argument(
+        "--email",
+        type=str,
+        help="Email address to use for Unpaywall API requests (alternatively, set UNPAYWALL_EMAIL environment variable)"
+    )
+    parser.add_argument(
+        "--download-dir",
+        type=str,
+        default="./downloads",
+        help="Directory to save downloaded PDFs (default: ./downloads)"
+    )
+    parser.add_argument(
+        "--data-download-dir",
+        type=str,
+        default="./data_downloads",
+        help="Directory to save downloaded data files (default: ./data_downloads)"
+    )
+    parser.add_argument(
+        "--target-formats",
+        type=str,
+        default=",".join(DEFAULT_DATA_FORMATS),
+        help=f"Comma-separated list of target data formats to download (default: {','.join(DEFAULT_DATA_FORMATS)})"
+    )
     args = parser.parse_args()
 
     if not args.dois and not args.input_file:
@@ -30,14 +53,20 @@ def main():
     dois = None
     if args.dois:
         dois = [doi.strip() for doi in args.dois.split(",")]
+    
+    target_formats = [fmt.strip() for fmt in args.target_formats.split(",")]
 
     df = process_and_analyze_dois(
         input_file=args.input_file,
         dois=dois,
-        save_to_disk=args.save_to_disk
+        save_to_disk=args.save_to_disk,
+        email=args.email,
+        download_dir=args.download_dir,
+        data_download_dir=args.data_download_dir,
+        target_formats=target_formats
     )
     print("Processing complete. Output saved to ecoopen_output.csv")
-    print(df[["doi", "data_availability_statements", "code_availability_statements"]])
+    print(df[["identifier", "doi", "title", "data_availability_statements", "all_data_availability_statements", "code_availability_statements", "all_code_availability_statements", "downloaded_data_files"]])
 
 if __name__ == "__main__":
     main()
--- EcoOpenPy/ecoopen/keywords.py
+++ EcoOpenPy/ecoopen/keywords.py
@@ -1,384 +1,137 @@
+# Keywords for EcoOpen package to identify data and code availability statements
 
-
-available = [
-    "included",
-    "deposited",
-    "released",
-    "is provided",
-    "are provided",
-    "contained in",
-    "available",
-    "reproduce",
-    "accessible",
-    "can be accessed",
-    "submitted",
-    "can be downloaded",
-    "reported in",
-    "uploaded",
-    "are public on"
-    "provided"
-    "archived"
-]
-
-was_available = [
-    "was provided",
-    "were provided",
-    "was contained in",
-    "were contained in",
-    "was available",
-    "were available",
-    "was accessible",
-    "were accessible",
-    "deposited by",
-    "were reproduced"
-]
-
-not_available = [
-    "not included",
-    "not deposited",
-    "not released",
-    "not provided",
-    "not contained in",
-    "not available",
-    "not accessible",
-    "not submitted"
-]
-
-field_specific_repo = [
-    "GEO",
-    "Deep Insight for Earth Science Data",
-    "ESS-DIVE",
-    "Gene Expression Omnibus",
-    "European Nucleotide Archive",
-    "National Center for Biotechnology Information",
-    "European Molecular Biology Laboratory",
-    "EMBL-EBI",
-    "BioProject",
-    "Sequence Read Archive",
-    "SRA",
-    "ENA",
-    "MassIVE",
-    "ProteomeXchange",
-    "Proteome Exchange",
-    "ProteomeExchange",
-    "MetaboLights",
-    "Array-Express",
-    "ArrayExpress",
-    "Array Express",
-    "PRIDE",
-    "DNA Data Bank of Japan",
-    "DDBJ",
-    "Genbank",
-    "Protein Databank",
-    "Protein Data Bank",
-    "PDB",
-    "Metagenomics Rapid Annotation using Subsystem Technology",
-    "MG-RAST",
-    "metabolights",
-    "OpenAgrar",
-    "Open Agrar",
-    "Electron microscopy data bank",
-    "emdb",
-    "Cambridge Crystallographic Data Centre",
-    "CCDC",
-    "Treebase",
-    "dbSNP",
-    "dbGaP",
-    "IntAct",
-    "ClinVar",
-    "European Variation Archive",
-    "dbVar",
-    "Mgnify",
-    "NCBI Trace Archive",
-    "NCBI Assembly",
-    "UniProtKB",
-    "Protein Circular Dichroism Data Bank",
-    "PCDDB",
-    "Worldwide Protein Data Bank",
-    "wwPDB",
-    "Structural Biology Data Grid",
-    "NeuroMorpho",
-    "G-Node",
-    "Neuroimaging Informatics Tools and Resources Collaboratory",
-    "NITRC",
-    "EBRAINS",
-    "GenomeRNAi",
-    "Database of Interacting Proteins",
-    "IntAct",
-    "Japanese Genotype-phenotype Archive",
-    "Biological General Repository for Interaction Datasets",
-    "Genomic Expression Archive",
-    "PeptideAtlas",
-    "Environmental Data Initiative",
-    "LTER Network Information System Data Portal",
-    "Global Biodiversity Information Facility",
-    "GBIF",
-    "Integrated Taxonomic Information System",
-    "ITIS",
-    "Knowledge Network for Biocomplexity",
-    "Morphobank",
-    "The Network Data Exchange",
-    "NDEx",
-    "FlowRepository",
-    "ImmPort",
-    "Image Data Resource",
-    "Eukaryotic Pathogen Database Resources",
-    "EuPathDB",
-    "Mouse Genome Informatics",
-    "Rat Genome Database",
-    "VectorBase",
-    "Xenbase",
-    "Zebrafish Model Organism Database",
-    "ZFIN",
-    "PhysioNet",
-    "Research Domain Criteria Database",
-    "RdoCdb",
-    "Synapse",
-    "UK Data Service",
-    "caNanoLab",
-    "ChEMBL",
-    "STRENDA",
-    "European Genome-phenome Archive",
-    "European Genome phenome Archive",
-    "Experimental Lakes Area",
-    "IISD – ELA",
-    "National Contaminants Information System",
-    "NCIS",
-    "Bedford Institute of Oceanography - Oceanographic Databases",
-    "Institut océanographique de Bedford - Bases de données océanographiques",
-    "Australian Urban Research Infrastructure Network",
-    "AURIN",
-    "Hakai Data",
-    "Institute Data Systems",
-    "Atlantic Canada Conservation Data Centre",
-    "AC CDC",
-    "Italian National Biodiversity Network",
-    "Network Nazionale Biodiversità",
-    "ForestPlots.net",
-    "The Vegetation Plot Archive Project",
-    "openLandscapes",
-    "The Knowledge Collection for Landscape Science",
-    "A vegetation data network for West Africa",
-    "Canadian Environmental Sustainability Indicators",
-    "CESI",
-    "PPBio Data Repository",
-    "Programa de Pesquisa em Biodiversidade Repositorio de Dados",
-    "Environmental Information Data Centre",
-    "EIDC",
-    "National Biodiversity Information System of Mexico",
-    "Sistema Nacional de Información sobre Biodiversidad de México",
-    "Australian SuperSite Network Data Portal",
-    "SupeSites Data Portal",
-    "DEIMS-SDR",
-    "Dynamic Ecological Information Management System",
-    "Tree Atlas Project",
-    "Marine Environmental Data Section",
-    "Open Archive for Miscellaneous Data",
-    "Biodiversity Exploratories Information System",
-    "BExIS",
-    "ICES data portal",
-    "International Council for the Exploration of the Sea dataset collections",
-    "Norwegian Marine Data Center",
-    "Norsk marint datasenter",
-    "Marine Microbial Database of India",
-    "National Harvest Survey",
-    "EarthWorks",
-    "Environmental Data Initiative Repository",
-    "EDI Data Portal",
-    "The European Data Portal",
-    "European data",
-    "data.europa.eu",
-    "Research Data Australia",
-    "ARDC",
-    "Santa Cruz Island Preserve Research Repository",
-    "Dangermond Preserve Research Repository",
-    "Draft Salmon Portal",
-    "Arctic Data Portal",
-    "NatCap",
-    "California's Marine Protected Area Monitoring Program",
-    "California MPA Monitoring Data Portal",
-    "The Ocean Data Integration Initiative Testing Portal",
-    "ODINI Testing Portal",
-    "Ocean Biodiversity Information System",
-    "OBIS",
-    "accession number",
-    "accession code",
-    "accession numbers",
-    "accession codes"
-]
-
-accession_nr = [
-    "G(SE|SM|DS|PL)[[:digit:]]{2,}", #GEO
-    "PRJ(E|D|N|EB|DB|NB)[:digit:]+",
-    "SAM(E|D|N)[A-Z]?[:digit:]+",
-    "[A-Z]{1}[:digit:]{4}", #GenBank
-    "[A-Z]{2}[:digit:]{6}",
-    "[A-Z]{3}[:digit:]{5}",
-    "[A-Z]{4,6}[:digit:]{3,}",
-    "GCA_[:digit:]{9}\\.[:digit:]+",
-    "SR(P|R|X|S|Z)[[:digit:]]{3,}",
-    "(E|P)-[A-Z]{4}-[:digit:]{1,}",
-    "[:digit:]{1}[A-Z]{1}[[:alnum:]]{2}",
-    "MTBLS[[:digit:]]{2,}",
-    "10.17590",
-    "10.5073",
-    "10.25493",
-    "10.6073",
-    "10.15468",
-    "10.5063",
-    "[[:digit:]]{6}",
-    "[A-Z]{2,3}_[:digit:]{5,}",
-    "[A-Z]{2,3}-[:digit:]{4,}",
-    "[A-Z]{2}[:digit:]{5}-[A-Z]{1}",
-    "DIP:[:digit:]{3}",
-    "FR-FCM-[[:alnum:]]{4}",
-    "ICPSR [:digit:]{4}",
-    "SN [:digit:]{4}"
-]
-
-repositories = [
-    "figshare",
-    "dryad",
-    "zenodo",
-    "dataverse",
-    "DataverseNL",
-    "osf",
-    "open science framework",
-    "mendeley data",
-    "GIGADB",
-    "GigaScience database",
-    "OpenNeuro",
-    "pangaea" # added by domagoj
-]
-
-github = ["github", "github.com", "github.io", "githubusercontent.com", "githubusercontent.io", "githubpages.com", "githubpages.io", "githubusercontent", "githubpages"]
-
-data = [
-    "data", 
-    "dataset",
-    "datasets"]
-
-all_data = [
-    "all data",
-    "raw data",
-    "full data set",
-    "full dataset",
-    "complete data set",
-    "complete dataset",
-]
-
-not_data = [
-    "not all data",
-    "no raw data",
-    "no full data set",
-    "no complete data set",
-    "no complete dataset",
-    "no full dataset"
-]
-
-source_code = [
-    "source code",
-    "analysis script",
-    "github",
-    "SAS script",
-    "SPSS script",
-    "R script",
-    "R code",
-    "python script",
-    "python code",
-    "matlab script",
-    "matlab code"
-]
-
-supplement = [
-    "supporting information",
-    "supplement",
-    "supplementary information",
-    "supplementary material",
-    "supplementary data"
-]
-
-file_formats = [
-    "csv",
-    "zip",
-    "xls",
-    "xlsx",
-    "sav",
-    "cif",
-    "fasta"
-]
-
-upon_request = [
-    "upon request",
-    "on request",
-    "upon reasonable request"
-]
-data_availability = [
-    "Data sharing",
-    "Data Availability Statement",
-    "Data Availability",
-    "Data deposition",
-    "Deposited Data",
-    "Data Archiving",
-    "Availability of data and materials",
-    "Availability of data",
-    "Data Accessibility",
-    "Data Accessibility Statement",
-    "Accessibility of data"]
-
-supplemental_table_name = [
-    "supplementary table",
-    "supplementary tables",
-    "supplemental table",
-    "supplemental tables",
-    "table", "tables",
-    "additional file",
-    "file",
-    "files"
-]
-
-supplemental_dataset = [
-    "supplementary data [[:digit:]]{1,2}",
-    "supplementary dataset [[:digit:]]{1,2}",
-    "supplementary data set [[:digit:]]{1,2}",
-    "supplemental data [[:digit:]]{1,2}",
-    "supplemental dataset [[:digit:]]{1,2}",
-    "supplemental data set [[:digit:]]{1,2}"
-]
-
-
-
-# original scripd binds dataset_name to dataset_numbering
-dataset_name = [
-    "data", "dataset", "datasets", "data set", "data sets"
-]
-
-dataset_number = ["S[[:digit:]]{1,2}"]
-
-# TODO: Check this!
-data_journal_dois = [
-    "10.1038/s41597-019-", "10.3390/data", "10.1016/j.dib"
-]
-    
 keywords = {
-    "available": available,
-    "was_available": was_available,
-    "not_available": not_available,
-    "field_specific_repo": field_specific_repo,
-    "accession_nr": accession_nr,
-    "repositories": repositories,
-    "github": github,
-    "data": data,
-    "all_data": all_data,
-    "not_data": not_data,
-    "source_code": source_code,
-    "supplement": supplement,
-    "file_formats": file_formats,
-    "upon_request": upon_request,
-    "data_availability": data_availability,
-    "supplemental_table_name": supplemental_table_name,
-    "supplemental_dataset": supplemental_dataset,
-    "dataset_name": dataset_name,
-    "dataset_number": dataset_number,
-    "data_journal_dois": data_journal_dois
+    "data_availability": [
+        "Data Availability",
+        "Availability of Data",
+        "Data Access",
+        "Supporting Information",
+        "Supplementary Information",
+    ],
+    "data": [
+        "data",
+        "dataset",
+        "datasets",
+        "raw data",
+        "processed data",
+        "supporting data",
+        "supplementary data",
+    ],
+    "all_data": [
+        "all data",
+        "complete data",
+        "full data",
+        "entire dataset",
+    ],
+    "dataset_name": [
+        "database",
+        "repository",
+        "archive",
+    ],
+    "available": [
+        "available",
+        "accessible",
+        "provided",
+        "shared",
+        "deposited",
+        "publicly available",
+        "freely available",
+    ],
+    "was_available": [
+        "was available",
+        "were available",
+        "made available",
+        "has been made available",
+        "have been made available",
+    ],
+    "not_available": [
+        "not available",
+        "unavailable",
+        "not shared",
+        "not provided",
+        "not accessible",
+        "not deposited",
+    ],
+    "not_data": [
+        "no data",
+        "no dataset",
+        "no datasets",
+        "no raw data",
+        "no processed data",
+    ],
+    "repositories": [
+        "Dryad",
+        "Zenodo",
+        "Figshare",
+        "Dataverse",
+        "Mendeley Data",
+        "OSF",
+        "Open Science Framework",
+    ],
+    "field_specific_repo": [
+        "GenBank",
+        "SRA",
+        "Sequence Read Archive",
+        "GEO",
+        "Gene Expression Omnibus",
+        "ArrayExpress",
+        "ENA",
+        "European Nucleotide Archive",
+    ],
+    "github": [
+        "GitHub",
+        "gitlab",
+        "bitbucket",
+    ],
+    "upon_request": [
+        "upon request",
+        "on request",
+        "by request",
+        "available upon reasonable request",
+        "available on reasonable request",
+    ],
+    "supplement": [
+        "supplementary material",
+        "supplemental material",
+        "supporting material",
+        "supplementary information",
+        "supplemental information",
+        "supporting information",
+        "supplementary data",
+        "supplemental data",
+        "supporting data",
+        "supplementary files",
+        "supplemental files",
+        "supporting files",
+    ],
+    "supplemental_table_name": [
+        "Table S[0-9]+",
+        "Figure S[0-9]+",
+        "Appendix S[0-9]+",
+        "Suppl Table [0-9]+",
+        "Suppl Fig [0-9]+",
+        "Supplementary Table [0-9]+",
+        "Supplementary Figure [0-9]+",
+    ],
+    "supplemental_dataset": [
+        "Dataset S[0-9]+",
+        "Suppl Dataset [0-9]+",
+        "Supplementary Dataset [0-9]+",
+    ],
+    "source_code": [
+        "source code",
+        "software",
+        "code",
+        "scripts",
+        "program",
+        "algorithm",
+    ],
+    "accession_nr": [
+        r"[A-Z][0-9]{6,8}",           # GenBank/EMBL/DDBJ (e.g., AF123456)
+        r"SRR[0-9]{6,}",              # SRA (Sequence Read Archive, e.g., SRR123456)
+        r"PRJ(NA|EB|DB)[0-9]{6,}",    # BioProject (e.g., PRJNA123456)
+        r"[A-Z]{2}[0-9]{6}",          # Another format (e.g., AB123456)
+        r"[A-Z]{1,2}_[0-9]{5,8}",     # Format with underscore (e.g., NC_123456)
+        r"[A-Z]{3}[0-9]{5,}",         # Another format (e.g., GEO12345)
+    ],
 }

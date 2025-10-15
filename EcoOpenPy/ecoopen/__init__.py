4044673920b9c1525a288fac17860cceeda0951c
Domagoj K. Hackenberger <domagojhack@gmail.com>
2025-10-15 13:05:11 +0200

    upgrade

--- EcoOpenPy/ecoopen/__init__.py
+++
@@ -1,6 +0,0 @@
-from ecoopen.ecoopen import process_and_analyze_dois, process_dois  # Absolute import
-from ecoopen.find_dois import find_dois  # Absolute import
-
-__version__ = "0.1.0"
-
-__all__ = ["process_and_analyze_dois", "process_dois", "find_dois"]
